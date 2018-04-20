3d5754ccdf79b8dc67fc6ca706e650c854ff6e46
hulk1999 <hungng300899@gmail.com>
2018-04-20 15:39:29 +0700

    update content & footer

---
+++ js/myScript.js
@@ -0,0 +1,9 @@
+function popupVideo(){
+	document.getElementById('popup-background').style.visibility = 'visible';
+	document.getElementById('popup-video').style.visibility = 'visible';
+}
+
+function closePopupVideo(){
+	document.getElementById('popup-background').style.visibility = 'hidden';
+	document.getElementById('popup-video').style.visibility = 'hidden';
+}
