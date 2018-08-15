efbce3871c52ada57cb2ecbc2f59357f37325a42
KomodoVu <37296951+KomodoVu@users.noreply.github.com>
2018-08-15 16:24:29 +0700

    Fix nav bar and contact form

--- js/c_resposive.js
+++ js/c_resposive.js
@@ -5,6 +5,8 @@
 $(".menu_hidden_c a").css("display", "none");
 
 $("#drop_down_menu_c").on("click", function(){
+	$(".menu_background_c").toggle();
+	$(".menu-background").toggle();
     $(".menu_hidden_c a").toggle();
-	$(".menu-background").toggleClass("menu_background_big_c");
+	$(".decoration_menu_c").toggle();
 });
